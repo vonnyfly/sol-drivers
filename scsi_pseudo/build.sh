b6d038309b12f15b3187066f86ca4c4535ef68dc
vonnyfly <lifeng1519@gmail.com>
2015-09-23 17:31:27 +0800

    update build.sh

--- scsi_pseudo/build.sh
+++ scsi_pseudo/build.sh
@@ -1,8 +1,19 @@
-$NAME=scsi_pseudo
-gcc -D_KERNEL -m64 -mcmodel=kernel -mno-red-zone -ffreestanding -nodefaultlibs -c $NAME.c
+#!/bin/sh
+
+arch=""
+extra=""
+if [[ `uname -i` = "i86pc" ]];then
+	arch="amd64"
+	extra="-mcmodel=kernel -mno-red-zone"
+else
+	arch="sparcv9"
+	extra="-mcpu=v9 -mcmodel=medlow -fno-pic -mno-fpu"
+fi
+NAME=scsi_pseudo
+gcc -D_KERNEL -m64 -ffreestanding -nodefaultlibs $extra -c $NAME.c
 ld -r -o $NAME $NAME.o
 
 cp $NAME.conf /usr/kernel/drv
 cp $NAME /tmp
-ln -sf /tmp/$NAME /usr/kernel/drv/amd64/$NAME
+ln -sf /tmp/$NAME /usr/kernel/drv/$arch/$NAME
 add_drv $NAME
